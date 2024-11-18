1d1815e262fba4f349fe9bebe5da71ecb7650460
vecasel <danife1986@hotmail.com>
2024-11-18 16:27:29 -0500

    cambios entityCatalog

--- scripts_procedimientos_almacenados/obtenerPermisosUsuario.sql
+++ scripts_procedimientos_almacenados/obtenerPermisosUsuario.sql
@@ -1,15 +1,11 @@
 USE [PRUEBA]
 GO
-
-/****** Object:  StoredProcedure [dbo].[spObtenerPermisosUsuario]    Script Date: 17/11/2024 16:54:20 ******/
+/****** Object:  StoredProcedure [dbo].[spObtenerPermisosUsuario]    Script Date: 18/11/2024 16:12:47 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
--- Procedimiento almacenado para obtener los permisos asignados a un usuario
-CREATE PROCEDURE [dbo].[spObtenerPermisosUsuario] (
+ALTER PROCEDURE [dbo].[spObtenerPermisosUsuario] (
     @idCatalogoEntidad INT, -- Identificador del catálogo de entidades
     @idUsuario BIGINT       -- Identificador del usuario
 )
@@ -24,11 +20,14 @@ BEGIN
         PuedeEliminar BIT,
         PuedeImportar BIT,
         PuedeExportar BIT,
-        EsRegistroEspecifico BIT
+        EsRegistroEspecifico BIT,
+		TipoUsuario NVARCHAR(20), 
+        RegistroID BIGINT,
+		Excluir BIT
     );
 
-    -- 1. Obtener permisos a nivel de entidad directamente asignados al usuario (PermiUser)
-    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico)
+    -- Obtener permisos a nivel de entidad directamente asignados al usuario (PermiUser)
+    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID)
     SELECT 
         p.name AS NombrePermiso,
         p.can_create AS PuedeCrear,
@@ -37,19 +36,21 @@ BEGIN
         p.can_delete AS PuedeEliminar,
         p.can_import AS PuedeImportar,
         p.can_export AS PuedeExportar,
-        0 AS EsRegistroEspecifico -- A nivel de entidad, no se aplica a registros específicos
+        0 AS EsRegistroEspecifico,
+		'user' AS TipoUsuario,
+        NULL AS RegistroID
     FROM 
         PermiUser pu
         INNER JOIN Permission p ON pu.permission_id = p.id_permi
     WHERE 
         pu.usercompany_id IN (
-            SELECT id_useco FROM UserCompany WHERE user_id = @idUsuario
+            SELECT id_useco FROM UserCompany WHERE id_useco = @idUsuario and useco_active = 1
         )
         AND pu.entitycatalog_id = @idCatalogoEntidad
-        AND pu.peusr_include = 1; -- Solo permisos incluidos
+        AND pu.peusr_include = 1;
 
-    -- 2. Obtener permisos a nivel de entidad asignados al rol del usuario (PermiRole)
-    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico)
+    -- Obtener permisos a nivel de entidad asignados al rol del usuario (PermiRole)
+    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID)
     SELECT 
         p.name AS NombrePermiso,
         p.can_create AS PuedeCrear,
@@ -58,64 +59,131 @@ BEGIN
         p.can_delete AS PuedeEliminar,
         p.can_import AS PuedeImportar,
         p.can_export AS PuedeExportar,
-        0 AS EsRegistroEspecifico -- A nivel de entidad, no se aplica a registros específicos
+        0 AS EsRegistroEspecifico,
+		'rol' AS TipoUsuario,
+        NULL AS RegistroID
     FROM 
         PermiRole pr
         INNER JOIN Permission p ON pr.permission_id = p.id_permi
     WHERE 
         pr.role_id IN (
-            SELECT role_id FROM UserCompany WHERE user_id = @idUsuario
+            SELECT role_id FROM [Role] WHERE role_id = @idUsuario and role_active = 1
         )
         AND pr.entitycatalog_id = @idCatalogoEntidad
-        AND pr.perol_include = 1; -- Solo permisos incluidos
+        AND pr.perol_include = 1;
 
-    -- 3. Obtener permisos específicos de registro asignados al usuario (PermiUserRecord)
-    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico)
-    SELECT 
-        p.name AS NombrePermiso,
-        p.can_create AS PuedeCrear,
-        p.can_read AS PuedeLeer,
-        p.can_update AS PuedeActualizar,
-        p.can_delete AS PuedeEliminar,
-        p.can_import AS PuedeImportar,
-        p.can_export AS PuedeExportar,
-        1 AS EsRegistroEspecifico -- Aplica a registros específicos
-    FROM 
-        PermiUserRecord pur
-        INNER JOIN Permission p ON pur.permission_id = p.id_permi
-    WHERE 
-        pur.usercompany_id IN (
-            SELECT id_useco FROM UserCompany WHERE user_id = @idUsuario
-        )
-        AND pur.entitycatalog_id = @idCatalogoEntidad
-        AND pur.peusr_record IS NOT NULL
-        AND pur.peusr_include = 1; -- Solo permisos incluidos
+    -- Permisos específicos de registro asignados al usuario (PermiUserRecord)
+    IF @idCatalogoEntidad = 1 -- Sucursal (BranchOffice)
+    BEGIN
+        INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID, Excluir)
+        SELECT 
+            p.name AS NombrePermiso,
+            p.can_create AS PuedeCrear,
+            p.can_read AS PuedeLeer,
+            p.can_update AS PuedeActualizar,
+            p.can_delete AS PuedeEliminar,
+            p.can_import AS PuedeImportar,
+            p.can_export AS PuedeExportar,
+            1 AS EsRegistroEspecifico,
+			'user' AS TipoUsuario,
+            bo.id_broff AS RegistroID,
+			CASE 
+				WHEN peusr_include = 1 THEN 0
+				WHEN peusr_include = 0 THEN 1 
+			END AS Excluir
+        FROM 
+            PermiUserRecord pur
+            INNER JOIN Permission p ON pur.permission_id = p.id_permi
+            INNER JOIN BranchOffice bo ON pur.peusr_record = bo.id_broff
+        WHERE 
+            pur.usercompany_id IN (
+                SELECT id_useco FROM UserCompany WHERE id_useco = @idUsuario and useco_active = 1
+            )
+            AND pur.entitycatalog_id = @idCatalogoEntidad
 
-    -- 4. Obtener permisos específicos de registro asignados al rol del usuario (PermiRoleRecord)
-    INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico)
-    SELECT 
-        p.name AS NombrePermiso,
-        p.can_create AS PuedeCrear,
-        p.can_read AS PuedeLeer,
-        p.can_update AS PuedeActualizar,
-        p.can_delete AS PuedeEliminar,
-        p.can_import AS PuedeImportar,
-        p.can_export AS PuedeExportar,
-        1 AS EsRegistroEspecifico -- Aplica a registros específicos
-    FROM 
-        PermiRoleRecord prr
-        INNER JOIN Permission p ON prr.permission_id = p.id_permi
-    WHERE 
-        prr.role_id IN (
-            SELECT role_id FROM UserCompany WHERE user_id = @idUsuario
-        )
-        AND prr.entitycatalog_id = @idCatalogoEntidad
-        AND prr.perrc_record IS NOT NULL
-        AND prr.perrc_include = 1; -- Solo permisos incluidos
+        -- Permisos específicos de registro asignados al rol del usuario (PermiRoleRecord)
+        INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID,Excluir)
+        SELECT 
+            p.name AS NombrePermiso,
+            p.can_create AS PuedeCrear,
+            p.can_read AS PuedeLeer,
+            p.can_update AS PuedeActualizar,
+            p.can_delete AS PuedeEliminar,
+            p.can_import AS PuedeImportar,
+            p.can_export AS PuedeExportar,
+            1 AS EsRegistroEspecifico,
+			'rol' AS TipoUsuario,
+            bo.id_broff AS RegistroID,
+			CASE 
+				WHEN perrc_include = 1 THEN 0
+				WHEN perrc_include = 0 THEN 1 
+			END AS Excluir
+        FROM 
+            PermiRoleRecord prr
+            INNER JOIN Permission p ON prr.permission_id = p.id_permi
+            INNER JOIN BranchOffice bo ON prr.perrc_record = bo.id_broff
+        WHERE 
+            prr.role_id IN (
+                SELECT role_id FROM [Role] WHERE role_id = @idUsuario and role_active = 1
+            )
+            AND prr.entitycatalog_id = @idCatalogoEntidad
+    END
+    ELSE IF @idCatalogoEntidad = 2 -- Centro de Costos (CostCenter)
+    BEGIN
+        INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID, Excluir)
+        SELECT 
+            p.name AS NombrePermiso,
+            p.can_create AS PuedeCrear,
+            p.can_read AS PuedeLeer,
+            p.can_update AS PuedeActualizar,
+            p.can_delete AS PuedeEliminar,
+            p.can_import AS PuedeImportar,
+            p.can_export AS PuedeExportar,
+            1 AS EsRegistroEspecifico,
+			'user' AS TipoUsuario,
+            cc.id_cosce AS RegistroID,
+			CASE 
+				WHEN peusr_include = 1 THEN 0
+				WHEN peusr_include = 0 THEN 1 
+			END AS Excluir
+        FROM 
+            PermiUserRecord pur
+            INNER JOIN Permission p ON pur.permission_id = p.id_permi
+            INNER JOIN CostCenter cc ON pur.peusr_record = cc.id_cosce
+        WHERE 
+            pur.usercompany_id IN (
+                SELECT id_useco FROM UserCompany WHERE id_useco = @idUsuario and useco_active = 1
+            )
+            AND pur.entitycatalog_id = 2
 
-    -- Devolver los permisos recopilados
+        -- Permisos específicos de registro asignados al rol del usuario (PermiRoleRecord)
+        INSERT INTO @tablaPermisos (NombrePermiso, PuedeCrear, PuedeLeer, PuedeActualizar, PuedeEliminar, PuedeImportar, PuedeExportar, EsRegistroEspecifico, TipoUsuario, RegistroID, Excluir)
+        SELECT 
+            p.name AS NombrePermiso,
+            p.can_create AS PuedeCrear,
+            p.can_read AS PuedeLeer,
+            p.can_update AS PuedeActualizar,
+            p.can_delete AS PuedeEliminar,
+            p.can_import AS PuedeImportar,
+            p.can_export AS PuedeExportar,
+            1 AS EsRegistroEspecifico,
+			'rol' AS TipoUsuario,
+            cc.id_cosce AS RegistroID,
+			CASE 
+				WHEN perrc_include = 1 THEN 0
+				WHEN perrc_include = 0 THEN 1 
+			END AS Excluir
+        FROM 
+            PermiRoleRecord prr
+            INNER JOIN Permission p ON prr.permission_id = p.id_permi
+            INNER JOIN CostCenter cc ON prr.perrc_record = cc.id_cosce
+        WHERE 
+            prr.role_id IN (
+                SELECT role_id FROM [Role] WHERE role_id = @idUsuario and role_active = 1
+            )
+            AND prr.entitycatalog_id = @idCatalogoEntidad
+    END
+
+    -- Devuelve los permisos recopilados
     SELECT * FROM @tablaPermisos;
 END;
-GO
-
-
